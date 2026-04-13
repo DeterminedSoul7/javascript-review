045650b8ebff756b47cf698b299f90fe30704f34
Renzo Bojorquez <u202411402@upc.edu.pe>
2026-04-13 11:30:15 -0500

    feat(product-id): add product-id value object.

---
+++ src/shared/domain/model/product-id.js
@@ -0,0 +1,19 @@
+import {generateUuid} from "./uuid.js";
+import {ValidationError} from "./errors.js";
+export class ProductId{
+    #value;
+    constructor(value) {
+        if (!validateUuid(value))
+            throw new ValidationError(`Invalid Product Id: ${value}. Must be a valid UUID`);
+        this.#value = value;
+    }
+    static generate () {
+        return new ProductId(generateUuid())
+    }
+
+    get value () {return this.#value;}
+
+    equals(other){
+        return other instanceof ProductId && this.#value === other.amount;
+    }
+}
