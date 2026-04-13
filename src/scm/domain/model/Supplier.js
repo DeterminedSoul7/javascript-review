67c2387707d70694f731c607c20a602029d2d08c
Renzo Bojorquez <u202411402@upc.edu.pe>
2026-04-13 12:08:17 -0500

    feat(supplier): add supplier aggregate.

---
+++ src/scm/domain/model/Supplier.js
@@ -0,0 +1,36 @@
+import {ValidationError} from "../../../shared/domain/model/errors.js";
+
+export class Supplier {
+    #id;
+    #name;
+    #contactEmail;
+    #lastOrderTotalPrice;
+
+    //Aqui comprobamos que se cumplan las invariants (reglas de negocio)
+    constructor({ id, name, contactEmail = null, lastOrderTotalPrice = null}) {
+        if (!(id instanceof SupplierId))
+            throw new ValidationError('Supplier Id must be a valid SupplierId object');
+        if (typeof name !== 'string' || name.length < 2 || name.length > 100)
+            throw new ValidationError('Supplier Name must be between 1 and 100 characters length.');
+        if (contactEmail !== null && !this.#isValidEmail(contactEmail))
+            throw new ValidationError('Supplier contact email must be a valid email address or null.');
+        if (lastOrderTotalPrice !== null && !(lastOrderTotalPrice instanceof Money))
+            throw new ValidationError('Last order total price must be a valid Money object or null.');
+
+    this.#id = id;
+    this.#name = name;
+    this.#contactEmail = contactEmail;
+    this.#lastOrderTotalPrice = lastOrderTotalPrice;
+    }
+    #isValidEmail(email){
+        const emailRegex = /^[^\s@]+@\.[^\s@]+\.[^\s@]+$/;
+        return emailRegex.test(email);
+    }
+    get id(){return this.#id;}
+
+    get name(){return this.#name;}
+
+    get contactEmail(){return this.#contactEmail;}
+
+    get lastOrderTotalPrice(){return this.#lastOrderTotalPrice;}
+}
